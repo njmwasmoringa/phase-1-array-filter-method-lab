102a058d55238404f6f0611c721c431722c562a4
Julius Mwangi <julius.mwangi@moringaschool.com>
2022-05-17 13:10:39 +0300

    commit tree 19b71a32a92f1e89fd664b8d867f336b7f3077b3

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+
+// name = 'luc'
+function findMatching(nameArray, name){
+    let results = nameArray.filter( (n)=>{
+        // n = 'Mohammed'
+        return n.substr(0, name.length).toLowerCase() == name.toLowerCase();
+        // return n.toLowerCase().includes(name);
+    } );
+
+    return results;
+}
+
+
+// const drivers = findMatching( ['Adam', 'Mohammed', 'Lucy', 'Jacob', 'Joseph lucky'], 'luc' );
+// console.table(drivers);
+
+
+function fuzzyMatch(nameArray, name){
+    let results = nameArray.filter( (n)=>{
+        // n = 'Mohammed'
+        return n.substr(0, name.length) == name;
+    } );
+
+    return results;
+}
+
+// nameArray = [ {name:'', hometown:''}, {name:'', hometown:''}, {name:'', hometown:''} ]
+function matchName(nameArray, name){
+    let results = nameArray.filter( (n)=>{
+        // n = '{name:'', hometown:''}'
+        return n.name.substr(0, name.length) == name;
+    } );
+
+    return results;
+}
